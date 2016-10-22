1168a93e001cc42256c09aa098ecf34e8509f6e6
fro59 <fernandoruedaoliva@gmail.com>
2016-10-22 14:17:47 +0200

    resumen inicial

--- js/app.js
+++ js/app.js
@@ -4,10 +4,22 @@ angular.module('portfolio', ['duParallax', 'ui.bootstrap']).
     $scope.background = parallaxHelper.createAnimator(-0.3);
 
     $scope.experiencies = [];
-    $scope.parallaxSrc = ['img/angular.png', 'img/docker.png', 'img/kubernetes.png',]
+    $scope.parallaxSrc = [];
 
     $http.get('/json/experiences.json').then(function(response){
     	$scope.experiencies = response.data;
+    }, function(response){
+    	console.log(response)
+    });
+
+    $http.get('/json/images.json').then(function(response){
+    	$scope.parallaxSrc = response.data;
+    }, function(response){
+    	console.log(response)
+    });
+
+    $http.get('/json/solutions.json').then(function(response){
+    	$scope.solutions = response.data;
     }, function(response){
     	console.log(response)
     });
